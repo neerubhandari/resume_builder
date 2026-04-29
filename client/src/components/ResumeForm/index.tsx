4b66358cef4ecc316d8d00d7234a9b4140c7990a
Niru Bhandari <nirubhandari45@gmail.com>
2026-04-29 18:30:45 +1000

    feat : db configuration done

--- client/src/components/ResumeForm/index.tsx
+++ client/src/components/ResumeForm/index.tsx
@@ -8,7 +8,6 @@ import SkillSection from "../sections/SkillsSection";
 
 const ResumeForm = ({
   formData,
-  handleSubmit,
   setFormData,
   validatePersonalInfo,
   setErrors,
@@ -26,6 +25,16 @@ const ResumeForm = ({
       setStep((prev) => prev + 1);
     }
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/api/resume", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+    console.log(formData, "after submitting");
+  };
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -34,6 +43,7 @@ const ResumeForm = ({
       <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
         <div className="flex items-center gap-2">
           <button
+            type="button"
             className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all false ${
               step === 5 ? "opacity-50 cursor-not-allowed" : ""
             }`}
@@ -43,6 +53,7 @@ const ResumeForm = ({
             Next
           </button>
           <button
+            type="button"
             className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all false ${
               step === 0 ? "opacity-50 cursor-not-allowed" : ""
             }`}
@@ -94,6 +105,7 @@ const ResumeForm = ({
       <button
         type="submit"
         className="bg-linear-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm"
+        onSubmit={handleSubmit}
       >
         Save Changes
       </button>
--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/db.js";
 const app = express();
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => res.send("Server is live"));
+app.post("/api/resume", (req, res) => {
+  const newResume = req.body;
+  console.log("received data", newResume);
+  res.status(201).json({ message: "Resume Created", data: newResume });
+});
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
