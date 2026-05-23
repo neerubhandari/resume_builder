6cfb69c20e17c412433d6cbca5984f8ecf673e21
Niru Bhandari <nirubhandari45@gmail.com>
2026-05-23 12:56:59 +1000

    feat : skill pattern added

---
+++ parser-service/nlp/skill_patterns.py
@@ -0,0 +1,18 @@
+SKILL_PATTERNS = [
+    "python",
+    "java",
+    "javascript",
+    "react",
+    "react js",
+    "node js",
+    "flask",
+    "django",
+    "sql",
+    "mongodb",
+    "aws",
+    "docker",
+    "machine learning",
+    "deep learning",
+    "data analysis",
+    "fastapi"
+]
